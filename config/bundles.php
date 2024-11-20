684ac7845c36615267504445521ba56bd2c01c56
loyet <thomas.loye@etu.umontpellier.fr>
2024-11-20 11:41:46 +0100

    feat : Suppression du cookie et invalidation du token (déconnexion)

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
